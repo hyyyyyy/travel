46a26416aaaa977aec4271cbae4db258e5330166
hyyyyyy <33621222+hyyyyyy@users.noreply.github.com>
2018-11-25 19:23:41 +0800

    20181125

--- src/page/home/components/Swiper.vue
+++ src/page/home/components/Swiper.vue
@@ -1,23 +1,14 @@
 <template>
-    <swiper :options="swiperOption">
-        <!-- slides -->
-        <div class="wrapper">
-            <swiper-slide>
-            <img  class="swiper-img" src="https://i0.hdslb.com/bfs/archive/65b2e89b47f30d0ba7c47bd2f42bf6bf49109923.jpg" alt="">
+     <swiper :options="swiperOption" ref="mySwiper" >
+    <!-- slides -->
+        <swiper-slide>
+            <img class="swiper-img" src="../../../assets/img/1.jpg" alt="">
             </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="https://i0.hdslb.com/bfs/sycp/tmaterial/201811/75f287dcde107d8939e5341039b39da7.jpg" alt="">
+        <swiper-slide>
+            <img class="swiper-img" src="../../../assets/img/2.jpg" alt="">
             </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="https://i0.hdslb.com/bfs/archive/9c8c50263ab3160ebf898a9749cd9a95493425e3.jpg" alt="">
-            </swiper-slide>
-            <swiper-slide>
-                <img class="swiper-img" src="https://i0.hdslb.com/bfs/archive/9fca0c5ca8fa9702b70173685bd5338e0771bfe7.jpg" alt="">
-            </swiper-slide>
-        </div>
-        
         <!-- Optional controls -->
-        <div class="swiper-pagination"  slot="pagination"></div>
+        <div class="swiper-scrollbar"   slot="scrollbar"></div>
     </swiper>
 </template>
 
@@ -27,20 +18,27 @@
         data () {
             return {
                 swiperOption: {
-                    pagination:'.swiper-pagination'
+                    pagination:'.swiper-pagination',
+                    loop: true
                 },
-                swiperList: []
+                swiperList: [
+                    
+                ]
             }
         }
     }
 </script>
 
 <style lang="stylus" scoped>
-    .weapper
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
         overflow: hidden
         width: 100%
-        padding-bottom: 2%
-    .swiper-img
-        width: 100%
+        height: 0
+        padding-bottom: 50%
+        background: #eee
+        .swiper-img
+            width: 100%
 </style>
 
